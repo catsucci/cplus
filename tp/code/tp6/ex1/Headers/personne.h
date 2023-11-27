848cca6810326a29319323b2bc1cc36818837660
catsucci <150689455+catsucci@users.noreply.github.com>
2023-11-27 14:32:46 +0100

    testing some lazyvim stuff

--- tp/code/tp6/ex1/Headers/personne.h
+++ tp/code/tp6/ex1/Headers/personne.h
@@ -1,23 +1,22 @@
 #ifndef PERSONNE_H
-#define PERSONNE_H 
+#define PERSONNE_H
 
 #include <cstring>
 
 class Personne {
 
-  public:
-    Personne ();
-    Personne (const char *, const char *, const int &);
-    Personne (const Personne &);
-    ~Personne();
-    Personne & operator = (const Personne &);
-    void affiche() const;
+public:
+  Personne();
+  Personne(const char *, const char *, const int &);
+  Personne(const Personne &);
+  ~Personne();
+  Personne &operator=(const Personne &);
+  void affiche() const;
 
-  private:
-    char * nom;
-    char * prenom;
-    int age;
+private:
+  char *nom;
+  char *prenom;
+  int age;
 };
 
-#endif // PERSONNE_H  
- 
+#endif // PERSONNE_H
--- tp/code/tp6/ex1/Sources/personne.cpp
+++ tp/code/tp6/ex1/Sources/personne.cpp
@@ -1,47 +1,47 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
 #include "../Headers/personne.h"
 
 Personne::Personne() {
-  this->nom = new char [50];
+  this->nom = new char[50];
   std::strcpy(this->nom, "null");
-  this->prenom = new char [50];
+  this->prenom = new char[50];
   std::strcpy(this->prenom, "null");
   this->age = 0;
   std::cout << "constr par def" << std::endl;
 }
 
-Personne::Personne (const char * nom, const char * prenom, const int & age) {
-  this->nom = new char [std::strlen(nom) + 1];
+Personne::Personne(const char *nom, const char *prenom, const int &age) {
+  this->nom = new char[std::strlen(nom) + 1];
   std::strcpy(this->nom, nom);
-  this->prenom = new char [std::strlen(prenom) + 1];
+  this->prenom = new char[std::strlen(prenom) + 1];
   std::strcpy(this->prenom, prenom);
   this->age = age;
   std::cout << "constr" << std::endl;
 }
 
-Personne::Personne (const Personne & templ) {
-  this->nom = new char [std::strlen(templ.nom) + 1];
+Personne::Personne(const Personne &templ) {
+  this->nom = new char[std::strlen(templ.nom) + 1];
   std::strcpy(this->nom, templ.nom);
-  this->prenom = new char [std::strlen(templ.prenom) + 1];
+  this->prenom = new char[std::strlen(templ.prenom) + 1];
   std::strcpy(this->prenom, templ.prenom);
   this->age = templ.age;
   std::cout << "sussy baka" << std::endl;
 }
 
 Personne::~Personne() {
-  delete [] nom;
-  delete [] prenom;
+  delete[] nom;
+  delete[] prenom;
   std::cout << "destruct" << std::endl;
 }
 
-Personne & Personne::operator = (const Personne & p) {
-  delete [] this->nom;
-  delete [] this->prenom;
-  this->nom = new char [std::strlen(p.nom) + 1];
+Personne &Personne::operator=(const Personne &p) {
+  delete[] this->nom;
+  delete[] this->prenom;
+  this->nom = new char[std::strlen(p.nom) + 1];
   std::strcpy(this->nom, p.nom);
-  this->prenom = new char [std::strlen(p.prenom) + 1];
+  this->prenom = new char[std::strlen(p.prenom) + 1];
   std::strcpy(this->prenom, p.prenom);
   this->age = p.age;
   std::cout << "fready fazbear" << std::endl;
