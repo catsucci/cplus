71eafb4a6ee3e034156e26060dccc2de8ef827a8
catsucci <150689455+catsucci@users.noreply.github.com>
2024-02-28 00:10:45 +0100

    Add clang-format into the root dir

--- tp/code/tp1/ex7/main.cpp
+++ tp/code/tp1/ex7/main.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
 
+using std::cin;
 using std::cout;
 using std::endl;
-using std::cin;
 
-int main ( int argc, char * argv[] ) {
+int
+main (int argc, char *argv[])
+{
     char z_operator;
     int leftOperand, rightOperand;
-    do {
-        cout << "Operator = ";
-        cin >> z_operator;
-    } while ( z_operator != '+' &&
-              z_operator != '-' &&
-              z_operator != '*' &&
-              z_operator != '/' );
+    do
+        {
+            cout << "Operator = ";
+            cin >> z_operator;
+        }
+    while (z_operator != '+' && z_operator != '-' && z_operator != '*'
+           && z_operator != '/');
     cout << "leftOperand = ";
     cin >> leftOperand;
     cout << "rightOperand = ";
     cin >> rightOperand;
-    switch ( z_operator ) {
+    switch (z_operator)
+        {
         case '+':
             cout << leftOperand << " " << z_operator << " " << rightOperand
-                << " = " << leftOperand + rightOperand << endl;
-        break;
+                 << " = " << leftOperand + rightOperand << endl;
+            break;
         case '-':
             cout << leftOperand << " " << z_operator << " " << rightOperand
-                << " = " << leftOperand - rightOperand << endl;
-        break;
+                 << " = " << leftOperand - rightOperand << endl;
+            break;
         case '*':
             cout << leftOperand << " " << z_operator << " " << rightOperand
-                << " = " << leftOperand * rightOperand << endl;
-        break;
+                 << " = " << leftOperand * rightOperand << endl;
+            break;
         case '/':
             cout << leftOperand << " " << z_operator << " " << rightOperand
-                << " = " << ( float ) leftOperand / rightOperand << endl;
-        break;
-    }
+                 << " = " << (float)leftOperand / rightOperand << endl;
+            break;
+        }
     return 0;
 }
