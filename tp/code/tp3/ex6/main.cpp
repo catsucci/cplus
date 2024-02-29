5a20d9f38551716ea274581c4b26ebf44b69b42a
catsucci <150689455+catsucci@users.noreply.github.com>
2024-02-29 11:30:04 +0100

    Apply GNU fomat

--- tp/code/tp3/ex6/main.cpp
+++ tp/code/tp3/ex6/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-void EchangeA( int &, int & );
-void EchangeB( int &, int & );
+void EchangeA (int &, int &);
+void EchangeB (int &, int &);
 
-int main( int argc, char *argv[] ) {
+int
+main (int argc, char *argv[])
+{
     int x, y;
     std::cout << "x = ";
     std::cin >> x;
@@ -12,21 +14,25 @@ int main( int argc, char *argv[] ) {
     std::cout << "Before swaping" << std::endl;
     std::cout << "x = " << x << " & y = " << y << std::endl;
     std::cout << "Swaping x & y" << std::endl;
-    EchangeA( x, y );
+    EchangeA (x, y);
     std::cout << "x = " << x << " & y = " << y << std::endl;
     std::cout << "Reswaping x & y" << std::endl;
-    EchangeB( x, y );
+    EchangeB (x, y);
     std::cout << "x = " << x << " & y = " << y << std::endl;
     return 0;
 }
 
-void EchangeA( int &x, int &y ) {
+void
+EchangeA (int &x, int &y)
+{
     x += y;
     y = x - y;
     x -= y;
 }
 
-void EchangeB( int &x, int &y ) {
+void
+EchangeB (int &x, int &y)
+{
     int *temp = new int;
     *temp = x;
     x = y;
